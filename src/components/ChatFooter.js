dede163739729753b90df3466e1d327590e6da85
Nitish <nitish0399@hotmail.com>
2022-02-22 10:28:14 +0530

    add share resource request modal

--- src/components/ChatFooter.js
+++ src/components/ChatFooter.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Modal} from 'react-bootstrap';
 import styles from "../styles/chat.module.css";
+import ShareResourceRequestModal from "./modals/ShareResourceRequestModal";
 import sendIcon from "../images/send-icon.svg";
 import photoIcon from "../images/photo-icon.svg";
 import videoIcon from "../images/video-icon.svg";
@@ -9,17 +9,18 @@ class ChatFooter extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      requestModal: false
+      requestModal: false,
+      resourceRequestType: "photo"
     };
 
-    this.sendAsset = this.sendAsset.bind(this);
+    this.shareResource = this.shareResource.bind(this);
     this.closeModal = this.closeModal.bind(this);
   }
 
   render() {
     let footerState = "d-block"; // footer visible
-    if (this.props.headerType != "Connected") {
-      footerState = "d-none";
+    if (this.props.headerType !== "Connected") {
+      footerState = "d-none"; // footer hidden
     }
 
     return (<div>
@@ -29,16 +30,16 @@ class ChatFooter extends React.Component {
           <button type="button" id={styles['send-btn']}><img src={sendIcon} alt="Send Icon"/></button>
         </div>
         <button type="button" id={styles['photo-upload-btn']}>
-          <img src={photoIcon} alt="Photo Icon" onClick={this.sendAsset}/></button>
+          <img src={photoIcon} alt="Photo Icon" onClick={() => this.shareResource("photo")}/></button>
         <button type="button" id={styles['video-upload-btn']}>
-          <img src={videoIcon} alt="Video Icon" onClick={this.sendAsset}/></button>
+          <img src={videoIcon} alt="Video Icon" onClick={() => this.shareResource("video")}/></button>
       </div>
-      <RequestModal show={this.state.requestModal} onHide={this.closeModal}/>
+      <ShareResourceRequestModal resourceType={this.state.resourceRequestType} show={this.state.requestModal} onHide={this.closeModal}/>
     </div>);
   }
 
-  sendAsset() {
-    this.setState({requestModal: true});
+  shareResource(resourceRequestType) {
+    this.setState({requestModal: true, resourceRequestType});
   }
 
   closeModal() {
@@ -46,18 +47,4 @@ class ChatFooter extends React.Component {
   }
 
 }
-function RequestModal(props) {
-  return (<Modal {...props} size="lg" aria-labelledby="contained-modal-title-vcenter" centered="centered">
-    <Modal.Body>
-      <h4>Centered Modal</h4>
-      <p>
-        Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-      </p>
-    </Modal.Body>
-    <Modal.Footer>
-      <button onClick={props.onHide}>Close</button>
-    </Modal.Footer>
-  </Modal>);
-}
-
 export default ChatFooter;
