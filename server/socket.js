d075d7cfd11e8c82cfa1d00f8552c9b15b38512a
Nitish <nitish0399@hotmail.com>
2022-03-09 23:26:10 +0530

    build chat logic for connecting and disconnecting

--- server/socket.js
+++ server/socket.js
@@ -4,12 +4,39 @@ module.exports = (io, socket) => {
 
   socket.on("chat:connect", msg => {
     console.log("user wants to connect");
+
+    if (usersAvailableToConnect.length != 0) {
+      // Pick random user
+      let randomStranger = usersAvailableToConnect[Math.floor(Math.random() * usersAvailableToConnect.length)];
+
+      // Map users
+      connectedUsers[socket.id] = randomStranger;
+      connectedUsers[randomStranger] = socket.id;
+
+      // Remove random stranger from usersAvailableToConnect array
+      removeUserFromAvailableList(randomStranger);
+
+      io.to(socket.id).emit("chat:connected");
+      io.to(randomStranger).emit("chat:connected");
+
+      return;
+    }
+
     usersAvailableToConnect.push(socket.id);
 
-    // TODO: Check for available users and connect to them randomly
+    const myTimeout = setTimeout(() => {
+      // If user is not connected within 10 seconds, emit unavailable
+      if (connectedUsers.hasOwnProperty(socket.id) == false) {
+        usersAvailableToConnect = [];
 
-    connectedUsers[socket.io] = xx; // xx=  randomly picked socket id;
-    connectedUsers[xx] = socket.io;
+        // Remove user from usersAvailableToConnect array
+        removeUserFromAvailableList(socket.id);
+
+        io.to(socket.id).emit("chat:unavailable");
+      }
+
+    }, 10000);
+
   });
 
   socket.on("chat:message", msg => {
@@ -17,18 +44,39 @@ module.exports = (io, socket) => {
 
     let senderSocketId = socket.id;
     let receiverSocketId = connectedUsers[senderSocketId];
-    if (receiverSocketId == null) {
-    }
+    if (receiverSocketId == null) {}
     io.to(receiverSocketId).emit(/* ... */);
   });
 
   socket.on("disconnect", () => {
     console.log("user disconnected");
 
+    // If user is connected to a stranger before disconnecting
+    if (connectedUsers.hasOwnProperty(socket.id)) {
+      let strangerUser = connectedUsers[socket.id];
+
+      delete connectedUsers[socket.id];
+      delete connectedUsers[strangerUser];
+
+      io.to(socket.id).emit("chat:disconnected");
+      io.to(strangerUser).emit("chat:disconnected");
+    }
+
     // Remove user from usersAvailableToConnect array
-    const index = array.indexOf(socket.id);
+    removeUserFromAvailableList(socket.id);
+
+    // Remove stranger from usersAvailableToConnect array
+    if (typeof(strangerUser) != "undefined") {
+      removeUserFromAvailableList(strangerUser);
+    }
+
+  });
+
+  function removeUserFromAvailableList(userSocketId) {
+    const index = array.indexOf(userSocketId);
     if (index > -1) {
       usersAvailableToConnect.splice(index, 1); // 2nd parameter means remove one item only
     }
-  });
+  }
+
 };
